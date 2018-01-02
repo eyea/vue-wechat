b13d55c05a67ebd11aa6c787ba68d416f96c8049
eyea <deyesea@gmail.com>
2018-01-03 00:13:34 +0800

    mock chat

--- src/components/Dialog/P2P.vue
+++ src/components/Dialog/P2P.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <!-- 注意不同的消息类型的分类 -->
-    <span class="showTime">{{new Date()}}</span>
+    <div class="showTime">{{new Date()}}</div>
 
     <div class='chatBox' v-for='(item, index) in latestInfos' :key='index'>
-      <img class='avator' v-bind:src='item.avatorUrl' alt="">
-      <p class="content">
-        {{ item.sendMessage.content}}
-      </p>
+      <div :class='item.isCurrentPeoson === 1 ? "fr" : "fl"'>
+        <img :class='item.isCurrentPeoson === 1 ? "fr" : "fl"' class='avator' v-bind:src='item.sendMessagePersonInfos.avatorUrl' alt="">
+        <p :class='item.isCurrentPeoson === 1 ? "fr" : "fl"' class='content'>
+          {{ item.sendMessage.content}}
+        </p>
+      </div>
     </div>
 
   </div>
@@ -24,42 +26,55 @@ export default {
           content: '明天去滑雪吧！'
         },
         isCurrentPeoson: 0, // 是否是当前的主人 1 是 0 否
-        sendMessagePersonInfos: [{ // 发送者相关信息 按照事件来排序
+        sendMessagePersonInfos: { // 发送者相关信息 按照事件来排序
           name: 'li',
           uid: 7758, // 通过该UID 点击头像可以查询到具体信息
           avatorUrl: 'https://avatars3.githubusercontent.com/u/17020223?s=200&u=a4eeebc47fe103d73123f8f44e97937580c6a4e3&v=4'
-        }, {
+        }
+      }, {
           time: '1514904883000',
           sendMessage: {
             type: 'text',
             content: '好啊~'
           },
           isCurrentPeoson: 1,
-          sendMessagePersonInfos: [{
+          sendMessagePersonInfos: {
             name: 'dong',
             uid: 520,
             avatorUrl: 'https://avatars3.githubusercontent.com/u/17020223?s=200&u=a4eeebc47fe103d73123f8f44e97937580c6a4e3&v=4'
-          }]
+          }
         }]
-      }]
     }
   }
 }
 </script>
 
 <style scoped>
+  .fl {
+    float: left;
+  }
+  .fr {
+    float: right;
+  }
   .showTime {
     display: inline-block;
     padding-top: 10px;
     padding-bottom: 10px;
+    width: 100%;
   }
   .chatBox {
-    width: 50%;
+    width: 100%;
+    height: 50px;
   }
   .avator {
     display: inline-block;
     width: 50px;
     height: 50px;
+  }
+  .content {
+    height: 50px;
+    line-height: 50px;
+    margin-left: 6px;
   }
 </style>
 
